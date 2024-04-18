b4780c4c0f7712aed3d52cb8d4e489815954d414
Clizio de Oliveira Guedes <clizioguedes@gmail.com>
2024-04-18 10:15:22 -0300

    Update environment variable in .env.example and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+  },
   compiler: {
     styledComponents: true
   }
